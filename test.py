c0e0d708e4d93771361bc1258100665663ec808e
vladimirfedotov <8Basket6>
2019-01-29 01:25:59 +0300

    1.8

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print('Привед!')
+# 1.8 -1
+x = int(input())
+
+print(x % (60 * 24) // 60)
+print(x%60)
+
+# 1.8 -2
+x = int(input())
+h = int(input())
+m = int(input())
+c = h * 60 + m + x
+print(c % (60 * 24) // 60)
+print(c % 60)
