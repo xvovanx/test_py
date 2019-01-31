30d92afd739f2a3ace1b911988a02670bfdd7270
vladimirfedotov <8Basket6>
2019-01-31 23:46:08 +0300

    1.12

--- test.py
+++ test.py
@@ -23,13 +23,104 @@
 # else:
 #     print('Это нормально')
 
-# 1.10 -2
+# # 1.10 -2
+# a = int(input())
+# b = ''
+# if a % 400 == 0:
+#     b = 'Високосный'
+# elif a % 4 == 0 and a % 100 !=0:
+#     b = 'Високосный'
+# else:
+#     b = 'Обычный'
+# print(b)
+
+# # 1.12 -1
+# import math
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# p = (a + b + c)/2
+# S = math.sqrt(p * (p - a) * (p - b) * (p - c))
+# print(S)
+
+# # 1.12 -2
+# a = int(input())
+# if (a > -15 and a <= 12) or (a > 14 and a < 17) or (a >= 19):
+#     print('True')
+# else:
+#     print('False')
+
+# # 1.12 -3
+# a = float(input())
+# b = float(input())
+# c = input()
+# if c == '+':
+#     print(a + b)
+# elif c == '-':
+#     print(a - b)
+# elif c == '/':
+#     if b == 0:
+#         print('Деление на 0!')
+#     else:
+#         print(a / b)
+# elif c == '*':
+#     print(a * b)
+# elif c == 'mod':
+#     if b == 0:
+#         print('Деление на 0!')
+#     else:
+#         print(a % b)
+# elif c == 'pow':
+#         print(a ** b)
+# elif c == 'div':
+#     if b == 0:
+#         print('Деление на 0!')
+#     else:
+#         print(a // b)
+
+# # 1.12 -4
+# import math
+# x = input()
+# if x == 'треугольник':
+#     a = float(input())
+#     b = float(input())
+#     c = float(input())
+#     p = (a + b + c)/2
+#     print(math.sqrt(p * (p - a) * (p - b) * (p - c)))
+# elif x == 'прямоугольник':
+#     a = float(input())
+#     b = float(input())
+#     print(a * b)
+# elif x == 'круг':
+#     r = float(input())
+#     print(3.14 * r**2)
+
+# 1.12 -5
 a = int(input())
-b = ''
-if a % 400 == 0:
-    b = 'Високосный'
-elif a % 4 == 0 and a % 100 !=0:
-    b = 'Високосный'
-else:
-    b = 'Обычный'
-print(b)
+b = int(input())
+c = int(input())
+max = 0
+min = 0
+ost = 0
+if a > b and b < c:
+    max = a
+    min = b
+    ost = c
+elif a > b and b > c:
+    if a > c:
+        max = a
+        min = c
+        ost = b
+    else:
+        max = c
+        min = b
+        ost = c
+elif b > a and a < c:
+    max = b
+    min = a
+    ost = c
+print(max)
+print(min)
+print(ost)
+
+
