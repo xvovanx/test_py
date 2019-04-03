2b8775553468fdd88ea36c77022647ef6fd5a998
vladimirfedotov <8Basket6>
2019-04-03 23:38:19 +0300

    2.3 -1

--- test.py
+++ test.py
@@ -184,9 +184,23 @@
 #         s = s + '\n' + str(b)
 # print(s)
 
+# # 2.3 -1
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# for g in range (c,d+1):
+#     print('\t'+str(g),end='')
+# print(end='\n')
+# for i in range (a,b+1):
+#     print(str(i)+'\t',end='')
+#     for j in range (c,d+1):
+#         print(str(i*j),end='\t')
+#     print(end='\n')
 
-
-
+# 2.3 -2
+a = int(input()
+b = int(input()
 
 
 
